81b6841229f7aa209689527b86bc778cb1e8e3c5
zacharynewton-wk <zachary.newton@workiva.com>
2018-02-09 07:47:19 -0600

    Start to implement DirectoryInput

--- src/DirectoryInput/DirectoryInput.js
+++ src/DirectoryInput/DirectoryInput.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './DirectoryInput.css';
+import ClassNameBuilder from '../lib/ClassNameBuilder';
 
 class DirectoryInput extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+      return nextProps.selectedDirectory !== this.props.selectedDirectory ||
+        nextProps.selectDirectory !== this.props.selectDirectory;
+  }
+
   render () {
+    let empty = !this.props.selectedDirectory;
+    let directoryDisplayClassNameBuilder = new ClassNameBuilder('directory-display');
+    directoryDisplayClassNameBuilder.add('empty', empty);
     return (
-      <div className="DirectoryInput">
+      <div className="DirectoryInput" onClick={() => this.selectDirectory()}>
+        <span className={directoryDisplayClassNameBuilder.className}>
+            {empty ? 'Select directory' : this.props.selectedDirectory}
+        </span>
+        <span className="directory-button">
+            <span className="icon icon-folder-open-g2"></span>
+        </span>
       </div>
     );
   }
+
+  selectDirectory() {
+    if (!remote.getCurrentWindow() || !remote.dialog) {
+        return new Error('No window or dialog created');
+    }
+    let path = remote.dialog.showOpenDialog(remote.getCurrentWindow(), {
+        properties: ['openDirectory', 'createDirectory']
+    });
+    this.props.selectDirectory(path ? path[0] : null);
+  }
 }
+
+DirectoryInput.propTypes = {
+    selectedDirectory: PropTypes.string,
+    selectDirectory: PropTypes.func
+};
 
 export default DirectoryInput;
